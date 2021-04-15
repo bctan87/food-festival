1f6a9f9b0d5391ea3a58aecae668a55cb056cf89
Ben Tan <btan87@gmail.com>
2021-04-15 12:00:25 -0700

    19.3.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require('path');
 const webpack = require("webpack");
 module.exports = {
@@ -8,9 +9,12 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
-      }),
-  ],
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
+  ],    
   mode: 'development'
 };
